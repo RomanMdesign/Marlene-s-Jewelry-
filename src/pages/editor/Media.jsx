34743285f2b27cd4bc48ad04cbb75802354f859d
RomanMdesign <piol.aljay.arroyo1@gmail.com>
2026-09-08 01:03:16 +0800

    Create Media.jsx

---
+++ src/pages/editor/Media.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Media() {
+
+  return (
+    <div className="page">
+
+      <h1>
+        Media
+      </h1>
+
+      <div className="card">
+
+        <h2>
+          Media Library
+        </h2>
+
+        <p>
+          Product images and website
+          media will be managed here.
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
