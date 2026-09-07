6a271e92a622d061ffe4daeb051d863dacad606f
RomanMdesign <piol.aljay.arroyo1@gmail.com>
2026-09-08 01:03:50 +0800

    Create Navigation.jsx

---
+++ src/pages/editor/Navigation.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Navigation() {
+
+  return (
+    <div className="page">
+
+      <h1>
+        Navigation
+      </h1>
+
+      <div className="card">
+
+        <h2>
+          Website Navigation
+        </h2>
+
+        <p>
+          Navigation management will
+          be implemented here.
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
