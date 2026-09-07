e462e74bfde626a1d9b5b4a8177773260fbe4504
RomanMdesign <piol.aljay.arroyo1@gmail.com>
2026-09-08 01:01:31 +0800

    Create HomePage.jsx

---
+++ src/pages/editor/HomePage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function HomePage() {
+
+  return (
+    <div className="page">
+
+      <h1>
+        Home Page
+      </h1>
+
+      <div className="card">
+
+        <h2>
+          Homepage Management
+        </h2>
+
+        <p>
+          Homepage content management
+          will be implemented here.
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
