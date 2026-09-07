5378a4dda05c706cf5d1342461d3581cd1299caf
RomanMdesign <piol.aljay.arroyo1@gmail.com>
2026-09-08 01:00:01 +0800

    Create WebsiteEditor.jsx

---
+++ src/pages/editor/WebsiteEditor.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function WebsiteEditor() {
+
+  return (
+    <div className="page">
+
+      <h1>
+        Website Editor
+      </h1>
+
+      <div className="card">
+
+        <h2>
+          Website Editor
+        </h2>
+
+        <p>
+          The actual website editing
+          system will be implemented here.
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
