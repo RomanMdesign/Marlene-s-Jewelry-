759d28fe3e94406df889f7d61acb19b6e020b971
RomanMdesign <piol.aljay.arroyo1@gmail.com>
2026-09-08 01:05:46 +0800

    Create Announcements.jsx

---
+++ src/pages/editor/Announcements.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Announcements() {
+
+  return (
+    <div className="page">
+
+      <h1>
+        Announcements
+      </h1>
+
+      <div className="card">
+
+        <h2>
+          Website Announcements
+        </h2>
+
+        <p>
+          Manage announcements and
+          promotional notices.
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
