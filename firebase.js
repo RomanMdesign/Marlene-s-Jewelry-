7f00ed43e351d76722bde07bdb584da5aab7ee8a
RomanMdesign <piol.aljay.arroyo1@gmail.com>
2026-09-08 02:27:15 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,36 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEE_FBCfLEVgovvGbbltgtQAL18jzgzyk",
   authDomain: "marlene-s-jewelry.firebaseapp.com",
+  databaseURL: "https://marlene-s-jewelry-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "marlene-s-jewelry",
   storageBucket: "marlene-s-jewelry.firebasestorage.app",
   messagingSenderId: "761899707259",
-  appId: "YOUR_APP_ID"
+  appId: "1:761899707259:web:5ce996ccbc3fce96836bde"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);,
+  authDomain: "marlene-s-jewelry.firebaseapp.com",
+  databaseURL:
+    "https://marlene-s-jewelry-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "marlene-s-jewelry",
+  storageBucket: "marlene-s-jewelry.firebasestorage.app",
+  messagingSenderId: "761899707259",
+  appId: "1:761899707259:web:5ce996ccbc3fce96836bde"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getDatabase(app);
