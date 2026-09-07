fdce29ffb6abffd340c6c94cc034bfc3db2c0fee
RomanMdesign <piol.aljay.arroyo1@gmail.com>
2026-09-08 00:55:59 +0800

    Create Account.jsx

---
+++ src/pages/customer/Account.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Account() {
+
+  return (
+    <div className="page">
+
+      <div className="page-heading">
+
+        <h1>
+          My Account
+        </h1>
+
+      </div>
+
+
+      <div className="card">
+
+        <h2>
+          Customer Information
+        </h2>
+
+        <p>
+          Name: Customer
+        </p>
+
+        <p>
+          Email: customer@example.com
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
