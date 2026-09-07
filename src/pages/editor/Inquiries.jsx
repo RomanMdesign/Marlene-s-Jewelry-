e0a520a465f6d0618d3087e78d2066c56747ed19
RomanMdesign <piol.aljay.arroyo1@gmail.com>
2026-09-08 01:02:23 +0800

    Create Inquiries.jsx

---
+++ src/pages/editor/Inquiries.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Inquiries() {
+
+  return (
+    <div className="page">
+
+      <h1>
+        Inquiries
+      </h1>
+
+      <div className="card">
+
+        <h2>
+          Customer Inquiries
+        </h2>
+
+        <p>
+          Customer inquiries will
+          appear here.
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
