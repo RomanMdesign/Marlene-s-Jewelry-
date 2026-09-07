62c740ec0fbc92bd675e90374a9e901ae16dfb49
RomanMdesign <piol.aljay.arroyo1@gmail.com>
2026-09-08 01:06:14 +0800

    Create ActivityLog.jsx

---
+++ src/pages/editor/ActivityLog.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function ActivityLog() {
+
+  return (
+    <div className="page">
+
+      <h1>
+        Activity Log
+      </h1>
+
+      <div className="card">
+
+        <h2>
+          System Activity
+        </h2>
+
+        <p>
+          Administrator activity will
+          be recorded here.
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
